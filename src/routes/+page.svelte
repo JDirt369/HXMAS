b886a3a1f8ed388ec195208d89a83824ad02a9ec
JoeDirt <83530206+JDirt369@users.noreply.github.com>
2022-10-28 06:53:18 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,3 @@
-<script>
-	import Counter from '$lib/Counter.svelte';
-</script>
-
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
 
 <section>
 	<h1></h1>
