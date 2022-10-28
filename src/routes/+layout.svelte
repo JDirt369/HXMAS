634ca370408c87d5098120042d94f022626146d3
JoeDirt <83530206+JDirt369@users.noreply.github.com>
2022-10-28 07:20:01 -0400

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<style>
 			body {
-				background-color: #2f2d2d;
+				background-color: #000000;
 			}
 			h1 {
 				font-family: Verdana, sans-serif;
